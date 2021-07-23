3dd192446d04a3cd9a15cc80ae701f316ea8dcca
Peter LaFosse <peter@vector35.com>
2021-07-23 08:18:40 -0400

    Updates for Binary Ninja 3.0 API changes

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import json
 import codecs
 import sys
 import logging
-import binaryninja
+from binaryninja.architecture import Architecture
+from binaryninja.platform import Platform
+from binaryninja.typelibrary import TypeLibrary
+from binaryninja.enums import StructureVariant
+from binaryninja.types import (Type, Pointer, Structure, Enumeration, Function)
 from argparse import ArgumentParser
 from pathlib import Path
 
-typelib = binaryninja.typelibrary.TypeLibrary.new(binaryninja.Architecture["x86_64"], "Win32")
-typelib.add_platform(binaryninja.Platform["windows-x86_64"])
-arch = binaryninja.Architecture["x86_64"]
-api_namespaces = {}
+typelib = TypeLibrary.new(Architecture["x86_64"], "Win32")
+typelib.add_platform(Platform["windows-x86_64"])
+arch = Architecture["x86_64"]
+assert arch is not None
 altnames = set()
 
 def kind_to_bn_type(kind):
@@ -21,38 +25,38 @@ def kind_to_bn_type(kind):
 
 def get_bn_type_from_name(name):
   if name == "Byte":
-    return binaryninja.types.Type.int(1, sign=False)
+    return Type.int(1, sign=False)
   elif name == "SByte":
-    return binaryninja.types.Type.int(1)
+    return Type.int(1)
   elif name == "Char":
-    return binaryninja.types.Type.char()
+    return Type.char()
   elif name == "UInt16":
-    return binaryninja.types.Type.int(2, sign=False)
+    return Type.int(2, sign=False)
   elif name == "Int16":
-    return binaryninja.types.Type.int(2)
+    return Type.int(2)
   elif name == "Int64":
-    return binaryninja.types.Type.int(8)
+    return Type.int(8)
   elif name == "UInt32":
-    return binaryninja.types.Type.int(4, sign=False)
+    return Type.int(4, sign=False)
   elif name == "UInt64":
-    return binaryninja.types.Type.int(8, sign=False)
+    return Type.int(8, sign=False)
   elif name == "Int32":
-    return binaryninja.types.Type.int(4)
+    return Type.int(4)
   elif name == "Single":
-    return binaryninja.types.Type.float(4)
+    return Type.float(4)
   elif name == "Double":
-    return binaryninja.types.Type.float(8)
+    return Type.float(8)
   elif name == "UIntPtr":
-    return binaryninja.types.Type.pointer(arch, binaryninja.types.Type.int(8, sign=False))
+    return Type.pointer(Type.int(8, sign=False), arch=arch)
   elif name == "IntPtr":
-    return binaryninja.types.Type.pointer(arch, binaryninja.types.Type.int(8, sign=True))
+    return Type.pointer(Type.int(8, sign=True), arch=arch)
   elif name == "Void":
-    return binaryninja.types.Type.void()
+    return Type.void()
   elif name == "Boolean":
-    return binaryninja.types.Type.bool()
+    return Type.bool()
   elif name == "Guid":
     #FIXME
-    return binaryninja.types.Type.void()
+    return Type.void()
   else:
     print(f"Unhandled Native Type: {name}")
     sys.exit(-1)
@@ -61,35 +65,33 @@ def handle_json_type(t):
   if t["Kind"] == "Native":
     return get_bn_type_from_name(t["Name"])
   if t["Kind"] == "PointerTo":
-    return binaryninja.types.Type.pointer(arch, handle_json_type(t["Child"]))
+    return Type.pointer(handle_json_type(t["Child"]), arch=arch)
   if t["Kind"] == "Array":
     if t["Shape"]:
-      return binaryninja.types.Type.array(handle_json_type(t["Child"]), int(t["Shape"]["Size"]))
+      return Type.array(handle_json_type(t["Child"]), int(t["Shape"]["Size"]))
     else:
-      return binaryninja.types.Type.pointer(arch, handle_json_type(t["Child"]))
+      return Type.pointer(handle_json_type(t["Child"]), arch=arch)
   if t["Kind"] == "ApiRef":
-    return binaryninja.types.Type.named_type(binaryninja.types.NamedTypeReference(name=t["Name"]))
+    return Type.named_type_reference(name=t["Name"])
   if t["Kind"] == "Struct":
     for nested_type in t["NestedTypes"]:
       typelib.add_named_type(nested_type["Name"], handle_json_type(nested_type))
-    new_struct = binaryninja.types.Structure()
+    new_struct = Structure()
     for field in t["Fields"]:
       child_type = handle_json_type(field["Type"])
-      new_struct.append(child_type, field["Name"])
-    return binaryninja.types.Type.structure_type(new_struct)
+      new_struct.append(type=child_type, name=field["Name"])
+    return new_struct
   if t["Kind"] == "LPArray":
-    return binaryninja.types.Type.pointer(arch, handle_json_type(t["Child"]))
+    return Type.pointer(handle_json_type(t["Child"]), arch=arch)
   if t["Kind"] == "Union":
     for nested_type in t["NestedTypes"]:
       typelib.add_named_type(nested_type["Name"], handle_json_type(nested_type))
-    new_union = binaryninja.types.Structure()
-    new_union.type = binaryninja.enums.StructureType.UnionStructureType
+    new_union = Structure(type=StructureVariant.UnionStructureType)
     for field in t["Fields"]:
-      child_type = handle_json_type(field["Type"])
-      new_union.append(child_type, field["Name"])
-    return binaryninja.types.Type.structure_type(new_union)
+      new_union.append(type=handle_json_type(field["Type"]), name=field["Name"])
+    return new_union
   if t["Kind"] == "MissingClrType":
-    return binaryninja.types.Type.void()
+    return Type.void()
   else:
     print(f"Unhandled type: {t}")
     sys.exit(0)
@@ -97,41 +99,30 @@ def handle_json_type(t):
 def create_bn_type_from_json(t):
   if t["Kind"] == "NativeTypedef":
     new_typedef = handle_json_type(t["Def"])
-    real_new_type = typelib.add_named_type(t["Name"], new_typedef)
+    typelib.add_named_type(t["Name"], new_typedef)
   elif t["Kind"] == "Enum":
-    new_enum = binaryninja.types.Enumeration()
-    for member in t["Values"]:
-      new_enum.append(member["Name"], int(member["Value"]))
-    real_new_type = binaryninja.types.Type.named_type_from_type(t["Name"], binaryninja.types.Type.enumeration_type(arch,new_enum))
-    typelib.add_named_type(t["Name"], real_new_type)
+    with Enumeration().builder(typelib, t["Name"]) as new_enum:
+      for member in t["Values"]:
+        new_enum.append(member["Name"], int(member["Value"]))
   elif t["Kind"] == "Struct":
     real_new_type = handle_json_type(t)
     typelib.add_named_type(t["Name"], real_new_type)
   elif t["Kind"] == "FunctionPointer":
-    ret_type = handle_json_type(t["ReturnType"])
-    param_list = []
-    for param in t["Params"]:
-      new_param = handle_json_type(param["Type"])
-      real_new_param = binaryninja.types.FunctionParameter(new_param, param["Name"])
-      param_list.append(real_new_param)
-    typelib.add_named_type(t["Name"], binaryninja.types.Type.pointer(arch, binaryninja.types.Type.function(ret_type, param_list)))
+    with Pointer(arch=arch).builder(typelib, t["Name"]) as pointer:
+      pointer.target = Function(handle_json_type(t["ReturnType"]))
+      for param in t["Params"]:
+        pointer.target.append(handle_json_type(param["Type"]), param["Name"])
   elif t["Kind"] == "Com":
-    new_struct = binaryninja.types.Structure()
-    for method in t["Methods"]:
-      ret_type = handle_json_type(method["ReturnType"])
-      param_list = []
-      for param in method["Params"]:
-        new_param = handle_json_type(param["Type"])
-        real_new_param = binaryninja.types.FunctionParameter(new_param, param["Name"])
-        param_list.append(real_new_param)
-      new_func = binaryninja.types.Type.function(ret_type, param_list)
-      new_struct.append(binaryninja.types.Type.pointer(arch, new_func), method["Name"])
-    typelib.add_named_type(t["Name"], binaryninja.types.Type.structure_type(new_struct))
+    with Structure().builder(typelib, t["Name"]) as new_struct:
+      for method in t["Methods"]:
+        func = Function(handle_json_type(method["ReturnType"]))
+        for param in method["Params"]:
+          func.append(handle_json_type(param["Type"]), param["Name"])
+        new_struct.append(type=Type.pointer(func, arch=arch), name=method["Name"])
   elif t["Kind"] == "ComClassID":
     return None
   elif t["Kind"] == "Union":
-    real_new_type = handle_json_type(t)
-    typelib.add_named_type(t["Name"], real_new_type)
+    typelib.add_named_type(t["Name"], handle_json_type(t))
     return None
   else:
     print(f"Found unknown type kind: {t['Kind']}")
@@ -142,10 +133,11 @@ def do_it(in_dir, out_file):
 
   files = p.glob("*.json")
 
+  api_namespaces = {}
   for file in files:
     api_namespaces[file.stem] = json.load(codecs.open(file, "r", "utf-8-sig"))
 
-  logging.info("Making a bunch of types...")
+  logging.info("Making a bunch of ..")
   i = 1
   for namespace in api_namespaces:
     metadata = api_namespaces[namespace]
@@ -166,14 +158,9 @@ def do_it(in_dir, out_file):
     funcs = metadata["Functions"]
     for f in funcs:
       altnames.add(f["DllImport"])
-      ret_type = handle_json_type(f["ReturnType"])
-      param_list = []
-      for param in f["Params"]:
-        new_param = handle_json_type(param["Type"])
-        real_new_param = binaryninja.types.FunctionParameter(new_param, param["Name"])
-        param_list.append(real_new_param)
-      new_func = binaryninja.types.Type.function(ret_type, param_list)
-      typelib.add_named_object(f["Name"], new_func)
+      with Function(handle_json_type(f["ReturnType"])).builder(typelib, f["Name"]) as func:
+        for param in f["Params"]:
+          func.append(handle_json_type(param["Type"]), param["Name"])
       func_count+=1
 
   for dll in altnames:
